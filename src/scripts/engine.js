b29ebe9166827b6ac76826ff9f98e340c9815210
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-12-28 11:24:37 -0300

    win condition

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -43,4 +43,17 @@ function handleCllick() {
 
 }
 
-function checkMatch() {}
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+  openCards = [];
+
+  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("You won!");
+  }
+}
